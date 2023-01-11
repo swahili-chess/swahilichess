32c66753f891a8574cf3cb6657a89454c818e43d
kilaladashina@gmail.com <kilaladashina@gmail.com>
2023-01-11 16:13:49 +0300

    Updated (11/01/2023)

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         <header className="header">
             <nav className="nav container">
                 <a href="index.html" className="nav__logo">
-                    <img src={logo} alt="" />
+                    <a href="#home" className="nav__logo">Chesswahili</a>
                 </a>
 
                 <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
