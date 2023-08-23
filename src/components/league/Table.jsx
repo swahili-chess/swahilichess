be4766d0ec54fb1402030da1f0b45522a1ffbde4
kilaladashina@gmail.com <kilaladashina@gmail.com>
2023-08-23 14:39:21 +0300

    Update meta tag content

--- src/components/league/Table.jsx
+++ src/components/league/Table.jsx
@@ -20,42 +20,42 @@ const Table = () => {
 
                             <li class="table-row1">
                                 <div class="col col-1">1.</div>
-                                <div class="col col-2">kafiti</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">6</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
                                 <div class="col col-6">0</div>
-                                <div class="col col-7">12</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row1">
                                 <div class="col col-1">2.</div>
-                                <div class="col col-2">hopertz</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">3</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">3</div>
-                                <div class="col col-7">6</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">3.</div>
-                                <div class="col col-2">tanzania-student</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">1</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">5</div>
-                                <div class="col col-7">2</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">4.</div>
-                                <div class="col col-2">klaus_gudy</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">1</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">5</div>
-                                <div class="col col-7">2</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
                         </ul>
 
@@ -73,42 +73,42 @@ const Table = () => {
 
                             <li class="table-row1">
                                 <div class="col col-1">1.</div>
-                                <div class="col col-2">dinokapon26</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">5</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">1</div>
-                                <div class="col col-7">10</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row1">
                                 <div class="col col-1">2.</div>
-                                <div class="col col-2">kazremy</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">3</div>
-                                <div class="col col-5">1</div>
-                                <div class="col col-6">2</div>
-                                <div class="col col-7">7</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
+                                <div class="col col-5">0</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">3.</div>
-                                <div class="col col-2">no_mane</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">3</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">3</div>
-                                <div class="col col-7">6</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">4.</div>
-                                <div class="col col-2">onesmo</div>
-                                <div class="col col-3">6</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
                                 <div class="col col-4">0</div>
-                                <div class="col col-5">1</div>
-                                <div class="col col-6">5</div>
-                                <div class="col col-7">1</div>
+                                <div class="col col-5">0</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
                         </ul>
 
@@ -126,41 +126,41 @@ const Table = () => {
 
                             <li class="table-row1">
                                 <div class="col col-1">1.</div>
-                                <div class="col col-2">bigfivewin05</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">5</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">1</div>
-                                <div class="col col-7">10</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row1">
-                                <div class="col col-1">1.</div>
-                                <div class="col col-2">innosaint</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">5</div>
+                                <div class="col col-1">2.</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">1</div>
-                                <div class="col col-7">10</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">3.</div>
-                                <div class="col col-2">arnie_n</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">1</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">5</div>
-                                <div class="col col-7">2</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">4.</div>
-                                <div class="col col-2">andre_beatz</div>
-                                <div class="col col-3">6</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
                                 <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">6</div>
+                                <div class="col col-6">0</div>
                                 <div class="col col-7">0</div>
                             </li>
                         </ul>
@@ -179,42 +179,42 @@ const Table = () => {
 
                             <li class="table-row1">
                                 <div class="col col-1">1.</div>
-                                <div class="col col-2">musir6363</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">5</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">1</div>
-                                <div class="col col-7">10</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row1">
-                                <div class="col col-1">1.</div>
-                                <div class="col col-2">alphazzz3r0</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">5</div>
+                                <div class="col col-1">2.</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">1</div>
-                                <div class="col col-7">10</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">3.</div>
-                                <div class="col col-2">hermamtm</div>
-                                <div class="col col-3">6</div>
-                                <div class="col col-4">2</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
                                 <div class="col col-5">0</div>
-                                <div class="col col-6">4</div>
-                                <div class="col col-7">4</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
 
                             <li class="table-row">
                                 <div class="col col-1">4.</div>
-                                <div class="col col-2">kdaxh</div>
-                                <div class="col col-3">-</div>
-                                <div class="col col-4">-</div>
-                                <div class="col col-5">-</div>
-                                <div class="col col-6">-</div>
-                                <div class="col col-7">-</div>
+                                <div class="col col-2">Player</div>
+                                <div class="col col-3">0</div>
+                                <div class="col col-4">0</div>
+                                <div class="col col-5">0</div>
+                                <div class="col col-6">0</div>
+                                <div class="col col-7">0</div>
                             </li>
                         </ul>
                     </h3>
