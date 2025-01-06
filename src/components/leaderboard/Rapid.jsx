0f3b7e43f325aaa322e6474bb2c4ceee351dd510
Lugano Abel <lugano.ngulwa@gmail.com>
2025-01-06 21:46:47 +0300

    Update url in  Rapid component

--- src/components/leaderboard/Rapid.jsx
+++ src/components/leaderboard/Rapid.jsx
@@ -5,7 +5,7 @@ const Rapid = () => {
 
 
     useEffect(() => {
-        fetch('https://api.swahilichess.com/lichess/leaderboard')
+        fetch('https://api.chess.tz/lichess/leaderboard')
             .then((response) => response.json())
             .then((json) => {
                 setRapid(json["rapid"]);
